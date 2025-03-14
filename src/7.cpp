e871250a796bff018ae5fc6a93f7e621c1b309b8
shizenvillafane169 <shizenvillafane169@gmail.com>
2025-03-14 18:17:56 +0000

    Add 7.cpp

---
+++ src/7.cpp
@@ -0,0 +1,13 @@
+
+// Random C++ Code
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+    int num = rand() % 10 + 1;
+    cout << "Random Number: " << num << endl;
+    return 0;
+}
