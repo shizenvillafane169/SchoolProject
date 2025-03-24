373800b760e1107c11a0dead784f43ac8dec404e
shizenvillafane169 <shizenvillafane169@gmail.com>
2025-03-24 12:13:10 +0000

    Add 16.cpp

---
+++ src/16.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    if (x > 10) {
+        cout << "Condition is true." << endl;
+    } else {
+        cout << "Condition is false." << endl;
+    }
+}
