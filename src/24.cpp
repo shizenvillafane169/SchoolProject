d0f0504b11c5d55965bc038b7b4c854493e73965
shizenvillafane169 <shizenvillafane169@gmail.com>
2025-04-06 00:47:19 +0000

    Add 24.cpp

---
+++ src/24.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    double b = 10.5;
+    string s1 = "Hello";
+    char c1 = 'A';
+    bool b1 = true;
+    
+    cout << "The value of a is: " << a << endl;
+    cout << "The value of b is: " << b << endl;
+    cout << "The type of s1 is: " << s1 << endl;
+    cout << "The type of c1 is: " << c1 << endl;
+    cout << "The type of b1 is: " << b1 << endl;
+
+    return 0;
+}
