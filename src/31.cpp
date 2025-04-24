1a268002cd199278bdf47ab1cc0799a440426537
shizenvillafane169 <shizenvillafane169@gmail.com>
2025-04-24 15:51:45 +0000

    Add 31.cpp

---
+++ src/31.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    double b = 10.5;
+    float c = 2.5f;
+    
+    cout << "The value of a is: " << a << endl;
+    cout << "The value of b is: " << b << endl;
+    cout << "The value of c is: " << c << endl;
+    
+    return 0;
+}
