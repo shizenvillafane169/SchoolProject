b468ed9a3c85a80d5b2b84a296b98f0d80ec674f
shizenvillafane169 <shizenvillafane169@gmail.com>
2025-05-23 14:01:12 +0000

    Add 43.cpp

---
+++ src/43.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num = 10;
+    if(num > 5) {
+        cout << "The number is greater than 5." << endl;
+    } else {
+        cout << "The number is not greater than 5." << endl;
+    }
+    return 0;
+}
